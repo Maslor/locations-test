96aa4014eaf9eddfc02c38e3005fdc256a27de7b
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-05-22 16:32:05 +0100

    Initial Commit

---
+++ locations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  locations
+//
+//  Created by Gabriel Freire on 22/05/16.
+//  Copyright © 2016 maslor. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
